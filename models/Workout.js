00dcd0501f39c50b1f93c21a078fd82a814e1ef0
Mikhail Mednikh <mmednikh@gmail.com>
2020-05-28 16:09:10 -0400

    password fixed

--- models/Workout.js
+++ models/Workout.js
@@ -4,71 +4,64 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  /* date: "Date",
-    totalDuration: "Total Workout Duration",
-    numExercises: "Exercises Performed",
-    totalWeight: "Total Weight Lifted",
-    totalSets: "Total Sets Performed",
-    totalReps: "Total Reps Performed",
-    totalDistance: "Total Distance Covered", */
-  day: {
-    type: Date,
-    default: Date.now(),
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Exercise Type is Required",
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Exercise Name is Required",
-      },
-      duration: {
-        type: Number,
-        trim: true,
-      },
-      weight: {
-        type: Number,
-        trim: true,
-      },
-      reps: {
-        type: Number,
-        trim: true,
-      },
-      sets: {
-        type: Number,
-        trim: true,
-      },
-      distance: {
-        type: Number,
-        trim: true,
-      },
+    day: {
+        type: Date,
+        default: Date.now(),
     },
-  ],
-  totalDuration: {
-    type: Number,
-  },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Exercise Type is Required",
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Exercise Name is Required",
+            },
+            duration: {
+                type: Number,
+                trim: true,
+            },
+            weight: {
+                type: Number,
+                trim: true,
+            },
+            reps: {
+                type: Number,
+                trim: true,
+            },
+            sets: {
+                type: Number,
+                trim: true,
+            },
+            distance: {
+                type: Number,
+                trim: true,
+            },
+        },
+    ],
+    totalDuration: {
+        type: Number,
+    },
 });
 
 WorkoutSchema.methods.setTotalDuration = function () {
-  let total = 0;
-  console.log("Help!");
-  this.exercises.forEach((exercise) => {
-    total += exercise.duration;
-  });
-  this.totalDuration = total;
-  console.log(this.totalDuration);
-  return this.totalDuration;
+    let total = 0;
+    console.log("Help!");
+    this.exercises.forEach((exercise) => {
+        total += exercise.duration;
+    });
+    this.totalDuration = total;
+    console.log(this.totalDuration);
+    return this.totalDuration;
 };
 
 WorkoutSchema.methods.addExercise = function (exercise) {
-  this.exercises.push(exercise);
-  this.setTotalDuration();
-  return this.save();
+    this.exercises.push(exercise);
+    this.setTotalDuration();
+    return this.save();
 };
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
